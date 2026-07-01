620aa577992d39652c4711c7709dfde00e48c25d
Test User <test@example.com>
2026-07-01 18:03:42 +0300

    Use absolute paths for template directories

--- barbsite/settings.py
+++ barbsite/settings.py
@@ -68,7 +68,9 @@ ROOT_URLCONF = 'barbsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            str(BASE_DIR / 'booking' / 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
